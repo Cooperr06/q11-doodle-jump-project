d07bbd467470334cb4401fb13d0cc7099166c6e7
therealnix <N_game@gmx.de>
2023-06-05 22:54:53 +0200

    Rendere 1.0

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,10 +1,8 @@
-import javax.imageio.ImageIO;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.nio.Buffer;
+
 
 public class Main {
 
--- src/src/Renderer.java
+++ src/src/Renderer.java
@@ -1,7 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
 
