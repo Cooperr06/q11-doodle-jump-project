f7d5302aea4f23e425a438916380a2145e7892f7
EnteSuessSauer <thisonerandom@gmail.com>
2023-06-10 16:50:52 +0200

    Getter & Setter for xVelocity and yVelocity

---
+++ src/util/Avatar.java
@@ -0,0 +1,27 @@
+package util;
+
+public class Avatar
+{
+    private int xVelocity;
+    private int yVelocity;
+
+    public int getXVelocity()
+    {
+        return xVelocity;
+    }
+
+    public void setXVelocity(int xVelocity)
+    {
+        this.xVelocity = xVelocity;
+    }
+
+    public int getYVelocity()
+    {
+        return yVelocity;
+    }
+
+    public void setYVelocity(int yVelocity)
+    {
+        this.yVelocity = yVelocity;
+    }
+}
