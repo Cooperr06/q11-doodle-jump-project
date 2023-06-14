9eb320da16dedee1ca0dc04cd1c441c4591bb190
benedikt <bene.forn05@gmail.com>
2023-06-14 11:10:17 +0200

    Attribute for DinoJump

---
+++ src/DinoJump.java
@@ -0,0 +1,12 @@
+public class DinoJump
+{
+    private DinoJump instance;
+    private InputManager inputManager;
+    private PlatformManger platformManger;
+    private Stage stage;
+    private Avatar avatar;
+    private DinoJump()
+    {
+
+    }
+}
