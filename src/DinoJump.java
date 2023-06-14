5269ba3838a7370835986b5cadb21bcc05c211cd
benedikt <bene.forn05@gmail.com>
2023-06-14 13:00:54 +0200

    getInstance(Skin skin, Position position) method implemented

--- src/DinoJump.java
+++ src/DinoJump.java
@@ -1,3 +1,6 @@
+import util.Avatar;
+import util.Position;
+
 public class DinoJump
 {
     private DinoJump instance;
@@ -6,6 +9,34 @@ public class DinoJump
     private Stage stage;
     private Avatar avatar;
     private DinoJump()
+    {
+
+    }
+
+    public void getInstance(Skin skin, Position position)
+    {
+        if(instance == null)
+        {
+            instance = new DinoJump();
+        }
+        if(inputManager == null)
+        {
+            inputManager = new InputManager();
+        }
+        if(stage == null)
+        {
+            stage = new Stage();
+        }
+        if(avatar == null)
+        {
+            avatar = new Avatar(skin, position);
+        }
+    }
+    public void start()
+    {
+        stage.showMainScreen();
+    }
+    public void startGameLoop()
     {
 
     }
