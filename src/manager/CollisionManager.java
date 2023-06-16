73a0957adca7ce3b9b303ce876f91b02823bb622
Meicky <mikychristl@icloud.com>
2023-06-16 08:41:10 +0200

    Collision Manager Implement

--- src/manager/CollisionManager.java
+++ src/manager/CollisionManager.java
@@ -1,4 +1,50 @@
 package manager;
 
-public class CollisionManager {
+import util.Avatar;
+import util.Platform;
+import util.Position;
+
+import java.util.ArrayList;
+
+public class CollisionManager
+{
+    private static CollisionManager instance;
+    private Avatar avatar;
+    private ArrayList<Platform> platforms;
+
+    private CollisionManager()
+    {
+
+    }
+
+    public static CollisionManager getInstance()
+    {
+        if (instance == null)
+        {
+            return new CollisionManager();
+        }
+        else
+        {
+            return instance;
+        }
+
+    }
+
+    public void setPlatforms(ArrayList<Platform> newPlatforms)
+    {
+        platforms = newPlatforms;
+    }
+
+    public void setAvatar(Avatar newAvatar)
+    {
+        Avatar = newAvatar;
+    }
+
+    public void checkForCollision()
+    {
+        Position avtPos = avatar.getPosition();
+        ArrayList<Position> platformPos = new ArrayList<Position>();
+    }
+
+
 }
