115017e8c1cd25cb28edce07dd08e0a9913690a5
niclas02 <niclas.p@t-online.de>
2023-06-18 18:12:59 +0200

    Added Platform Manager and a Prototype for the Enemy Manager

--- src/util/PlatformManager.java
+++ src/util/PlatformManager.java
@@ -1,41 +1,61 @@
 package util;
 import java.util.Random;
+import java.util.List;
+import java.util.LinkedList;
 public class PlatformManager
 {
-    // Draft of a class using the singleton pattern
-    private List platforms;
+    /** Draft of a class using the singleton pattern*/
+    private List<Platform> platforms;
     private Random rnGenerator;
-    private static PlatformManager pManager = new PlatformManager();
+    private int windowX;
+    private int windowY;
+
+    public static PlatformManager platformManager = new PlatformManager();
+
 
     private PlatformManager()
     {
-        platforms = new List();
+        platforms = new LinkedList<Platform>();
         rnGenerator = new Random();
+        windowX = 720;
+        windowY = 1280;
     }
-    public static PlatformManager returnpManager()
+    public static PlatformManager returnplatformManager()
     {
-        return pManager;
+        return platformManager;
     }
+
+
+
+
     public Platform generatePlatform()
     {
-        // Asking the List to return the Platform with the highest y value
-        Platform pComparison = platforms.giveLastPlatform();
-        // gaining a random number for x and y
-        int y = rnGenerator.nextInt(20);
-        int x = rnGenerator.nextInt(80);
         Platform pnew;
-        // if not then the game just began -> creating a platform a bit above the avatar
-        if(pComparison == null)
+        if(platforms.get(0).getPosition().getY()<720)
         {
-            pnew = new Platform(skin, new Position(x,y));
+            platforms.remove(0);
+            pnew = platforms.get(0);
         }
-        else
-        // creating a platform above the last
-        {
-            Position position = pComparison. getPosition();
-            int yOld = position.getY();
-            pnew = new Platform(skin, new Position (x,yOld + y);
-        }
-        return pnew;
+        /** Asking the List to return the Platform with the highest y value */
+
+            Platform pComparison = platforms.get(platforms.size());
+            /** gaining a random number for x and y */
+            int y = rnGenerator.nextInt(100)+20;
+            int x = rnGenerator.nextInt(720);
+            /** if not then the game just began -> creating a platform a bit above the avatar */
+            if(pComparison == null)
+            {
+                pnew = new Platform(skin, new Position(360,1200));
+            }
+            else
+            /** creating a platform above the last */
+            {
+                Position position = pComparison. getPosition();
+                int yOld = position.getY();
+                pnew = new Platform(skin, new Position (x,yOld + y);
+
+            }
+            return pnew;
+
     }
 }
