9344212583a0099db44976ea13bfcafef2e08b2c
benedikt <bene.forn05@gmail.com>
2023-06-18 20:35:47 +0200

    reformated code

--- src/DinoJump.java
+++ src/DinoJump.java
@@ -5,26 +5,31 @@ import util.Position;
 import util.Skin;
 import util.Stage;
 
-public class DinoJump {
+public class DinoJump
+{
     private static DinoJump instance;
     private InputManager inputManager;
     private PlatformManger platformManger;
     private Stage stage;
     private Avatar avatar;
 
-    private DinoJump() {
+    private DinoJump()
+    {
         inputManager = new PlatformManager();
         stage = new Stage();
         avatar = new Avatar(Skin.of(0), new Position(0, 0));
     }
 
-    public static void getInstance() {
-        if (instance == null) {
+    public static void getInstance()
+    {
+        if (instance == null)
+        {
             new DinoJump();
         }
     }
 
-    public void start() {
+    public void start()
+    {
         stage.showMainScreen();
     }
 }
