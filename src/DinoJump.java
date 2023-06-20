2e74591bb7bdee8d9585720c5a2908811fdeae26
benedikt <bene.forn05@gmail.com>
2023-06-20 13:52:16 +0200

    updated reviewed class

--- src/DinoJump.java
+++ src/DinoJump.java
@@ -1,5 +1,3 @@
-import manager.InputManager;
-import manager.PlatformManager;
 import util.Avatar;
 import util.Position;
 import util.Skin;
@@ -8,14 +6,11 @@ import util.Stage;
 public class DinoJump
 {
     private static DinoJump instance;
-    private InputManager inputManager;
-    private PlatformManger platformManger;
     private Stage stage;
     private Avatar avatar;
 
     private DinoJump()
     {
-        inputManager = new PlatformManager();
         stage = new Stage();
         avatar = new Avatar(Skin.of(0), new Position(0, 0));
     }
